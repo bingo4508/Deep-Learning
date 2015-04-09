b20fbb8c118554ad64808a21f211b0f3c70b6e86
bingo4508 <bingo4508@gmail.com>
2015-04-09 22:52:24 +0800

    add README, learning_rate_decay

---
+++ README.md
@@ -0,0 +1,50 @@
+Deep-Learning
+========================
+
+##Environment:
+-------------------------
+Linux with BLAS, LAPACK installed
+
+
+
+##Package dependency:
+-------------------------
+1. Armadillo C++ linear algebra library
+2. BLAS
+3. LAPACK
+
+
+
+##How to compile:
+-------------------------
+cd src
+sh compile.sh
+
+
+
+##How to run:
+-------------------------
+Before training, use the following to merge data and label, if label is not
+0~N, please supply out_map to save the mapping of  string label to numeric
+label(0~N).
+
+> script/merge_data_label.py train.ark train.lab new_train.ark [out_map]
+
+Each epoch will save the model to output_model, if load_model supplied, will
+load that model first and start training.
+
+> bin/train learning_rate(0.01) batch_size(10) structure(5-4-3) max_epoch(100) new_train.ark output_model [load_model]
+
+if the .ark has answer(new_train.ark) has_answer=1, if not(test.ark)
+has_answer=0
+> bin/predict test_file model_name raw_result has_answer(1/0)
+
+
+##For Kaggle Sumission
+1. 48_phone->39_phone
+   map_phone_label.py raw_result final_result out_map 48-39.map
+
+2. 1942_state->39_phone
+   map_state_label.py raw_result final_result state-48-39.map
+
+Upload final_result
--- src/train.cpp
+++ src/train.cpp
@@ -11,81 +11,85 @@ using namespace std;
 
 
 int main(int argc, char** argv){
-	clock_t t1, t2;
-	double learning_rate;
-	int batch_size;
-	vector<int> layers;
-	string train_fname;
-	int max_epoch;
-	float valid_ratio=0.1;
-	string output_model;
-	string structure;
-	Net d;
+        clock_t t1, t2;
+        double learning_rate;
+        double learning_rate_decay;
+        int batch_size;
+        vector<int> layers;
+        string train_fname;
+        int max_epoch;
+        float valid_ratio=0.1;
+        string output_model;
+        string structure;
+        Net d;
 
-	//Set parameters
-	//ex: ./run 0.01 5-4-3 300 train_file_name output_model_name model_name
-	if(argc < 7){
-		printf("Usage:\n");
-		printf("./train learning_rate(0.01) batch_size(10) structure(5-4-3) max_epoch(100) train_file output_model [load_model]\n");
-		return 0;
-	}else{
-		learning_rate = atof(argv[1]);
-		batch_size = atoi(argv[2]);
-		string lyr(argv[3]);
-		structure = lyr.c_str();
-		vector<string> x = split(lyr,"-");
-		for(int i=0;i<x.size();i++){
-			layers.push_back(atoi(x[i].c_str()));
-		}
-		max_epoch = atoi(argv[4]);
-		train_fname.assign(argv[5]);
-		output_model.assign(argv[6]);
-	}
+        //Set parameters
+        //ex: ./run 0.01 5-4-3 300 train_file_name output_model_name model_name
+        if(argc < 8){
+                printf("Usage:\n");
+                printf("./train learning_rate(0.01) learning_rate_decay(0.8) batch_size(10) structure(5-4-3) max_epoch(100) train_file output_model [load_model]\n");
+                return 0;
+        }else{
+                learning_rate = atof(argv[1]);
+                learning_rate_decay = atof(argv[2]);
+                batch_size = atoi(argv[3]);
+                string lyr(argv[4]);
+                structure = lyr.c_str();
+                vector<string> x = split(lyr,"-");
+                for(int i=0;i<x.size();i++){
+                        layers.push_back(atoi(x[i].c_str()));
+                }
+                max_epoch = atoi(argv[5]);
+                train_fname.assign(argv[6]);
+                output_model.assign(argv[7]);
+        }
 
-	//Initialize neural network
-	if(argc == 7){
-		d.load_model(layers);
-	}else if(argc == 8){
-		string m_name(argv[7]);
-		d.load_model(m_name);
-	}else{
-		printf("wrong parameters\n");
-		return 0;
-	}
-	d.learning_rate = learning_rate;
-	d.batch_size = batch_size;
+        //Initialize neural network
+        if(argc == 8){
+                d.load_model(layers);
+        }else if(argc == 9){
+                string m_name(argv[7]);
+                d.load_model(m_name);
+        }else{
+                printf("wrong parameters\n");
+                return 0;
+        }
+        d.learning_rate = learning_rate;
+        d.batch_size = batch_size;
 
 
-	//Loading data
-	puts("Loading training data...");
-	t1 = clock();
-	d.load_train_data(train_fname,d.data,d.label,d.index);
-	t2 = clock();
-	printf("spent %f secs\n", GET_SECS(t1,t2));
+        //Loading data
+        puts("Loading training data...");
+        t1 = clock();
+        d.load_train_data(train_fname,d.data,d.label,d.index);
+        t2 = clock();
+        printf("spent %f secs\n", GET_SECS(t1,t2));
 
-	vector<int> valid_index(d.index.begin(), d.index.begin()+d.index.size()*valid_ratio);
-	vector<int> train_index(d.index.begin()+d.index.size()*valid_ratio, d.index.end());
+        vector<int> valid_index(d.index.begin(), d.index.begin()+d.index.size()*valid_ratio);
+        vector<int> train_index(d.index.begin()+d.index.size()*valid_ratio, d.index.end());
 
-	//Training
-	puts("Start training...");
-	for(int epoch=0;epoch<max_epoch;epoch++){
-		t1 = clock();
+        //Training
+        puts("Start training...");
+        for(int epoch=0;epoch<max_epoch;epoch++){
+                t1 = clock();
 
-		random_shuffle(train_index.begin(), train_index.end());
-		int j=0;
-		for(vector<int>::iterator it=train_index.begin();it!=train_index.end();++it,++j){
-			mat y = zeros<mat>(layers.back(),1);
-			d.feedforward(d.data[*it]);
-			y(d.label[*it],0) = 1;
-			d.backprop(y);
-			if((j % d.batch_size == 0) && j != 0)
-				d.update();
-		}
-		float train_err = d.report_error_rate(d.data,d.label, train_index);
-		float valid_err = d.report_error_rate(d.data,d.label, valid_index);
+                random_shuffle(train_index.begin(), train_index.end());
+                int j=0;
+                for(vector<int>::iterator it=train_index.begin();it!=train_index.end();++it,++j){
+                        mat y = zeros<mat>(layers.back(),1);
+                        d.feedforward(d.data[*it]);
+                        y(d.label[*it],0) = 1;
+                        d.backprop(y);
+                        if((j % d.batch_size == 0) && j != 0)
+                                d.update();
+                }
+                float train_err = d.report_error_rate(d.data,d.label, train_index);
+                float valid_err = d.report_error_rate(d.data,d.label, valid_index);
 
-		t2 = clock();
-		printf("epoch %d\ttrain err:%f\tvalid err:%f\t%f secs\n", epoch,train_err,valid_err,GET_SECS(t1,t2));
-		d.save_model(output_model, structure);
-	}
+                t2 = clock();
+                printf("epoch %d\ttrain err:%f\tvalid err:%f\t%f secs\n", epoch,train_err,valid_err,GET_SECS(t1,t2));
+                d.save_model(output_model, structure);
+                d.learning_rate *= learning_rate_decay;
+        }
 }
+
