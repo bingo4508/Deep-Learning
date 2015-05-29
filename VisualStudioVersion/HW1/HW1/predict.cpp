4d84baa6cf5a231b7b550f71103f3e2662f11a7e
alex1230608 <alex.eed99@nctu.edu.tw>
2015-05-29 10:10:57 +0800

    visual with softmax, overflow fixed, cluster speed up

--- VisualStudioVersion/HW1/HW1/predict.cpp
+++ VisualStudioVersion/HW1/HW1/predict.cpp
@@ -2,34 +2,41 @@
 #include <algorithm>
 #include <stdio.h>
 #include <assert.h>
-#include "Net.h"
+#include "nnet.h"
 
 #define PAUSE printf("Press Enter key to continue..."); fgetc(stdin);
 
 using namespace std;
 
 
-int ___main(int argc, char** argv){
+int main2(int argc, char** argv){
 	double learning_rate;
 	vector<int> layers;
 	string test_fname;
 	string model;
 	string result;
-	Net d;
+	int has_answer;
+	NNet d;
 
 	//Set parameters
-	//ex: ./predict test_file_name model_name output_name
-	assert(argc == 4);
-	test_fname.assign(argv[1]);
-	model.assign(argv[2]);
-	result.assign(argv[3]);
+	//ex: ./predict test_file_name model_name output_name has_answer
+	if(argc == 5){
+		test_fname.assign(argv[1]);
+		model.assign(argv[2]);
+		result.assign(argv[3]);
+		has_answer = atoi(argv[4]);
+	}else{
+		printf("Usage:\n");
+		printf("./predict test_file model_name output_name has_answer(1/0)\n");
+		return 0;
+	}
 	
 
 	//Initialize neural network
 	d.load_model(model);
 
 	puts("Start predicting...");
-	d.predict(test_fname,result);
+	d.predict(test_fname,result,has_answer);
 
 	return 0;
 }
